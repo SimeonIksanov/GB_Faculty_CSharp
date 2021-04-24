05b80d73a37772ae4a88c23bdff7ff15171e5ab4
Simeon <ext3@mail.ru>
2021-04-24 17:32:31 +0500

    simple howework for lesson01

---
+++ CSharp_Introduction/Lesson01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как ваше имя?: ");
+            string username = Console.ReadLine();
+            string todayDate = DateTime.Today.ToString("dd-MM-yyyy");
+            Console.WriteLine($"Привет, {username}, сегодня {todayDate}");
+        }
+    }
+}
