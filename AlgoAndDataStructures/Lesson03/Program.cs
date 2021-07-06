e9ad95623336ce6ef6630d6fce9980dfef7e76d2
Simeon Iksanov <simeon.iksanov@gmail.com>
2021-07-06 17:22:29 +0500

    Algo and Structures: Lesson 03 Homework

---
+++ AlgoAndDataStructures/Lesson03/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Lesson03
+{
+    class Program
+    {
+        private const int count = 900_000_000;
+        static PointClass[] pointClass = new PointClass[count];
+        static PointStructD[] pointStructD = new PointStructD[count];
+        static PointStructF[] pointStructF = new PointStructF[count];
+
+        static void Main(string[] args)
+        {
+            //temp.runTemp();
+
+            FillArrays();
+
+            Console.WriteLine($"Кол-во повторений в цикле: {count}\n" +
+                $"Обычный метод расчёта дистанции со ссылочным типом(float): {MyBenchmarkRunner.Start(BulkGetDistanceOnClass)}\n" +
+                $"Обычный метод расчёта дистанции со значимым типом(float) : {MyBenchmarkRunner.Start(BulkGetDistanceOnStructF)}\n" +
+                $"Обычный метод расчёта дистанции со значимым типом(double): {MyBenchmarkRunner.Start(BulkGetDistanceOnStructD)}\n" +
+                $"Расчёт дистанции без квадратного корня со значимым типом : {MyBenchmarkRunner.Start(BulkGetFastDistanceOnStructF)}");
+        }
+
+        private static void FillArrays()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                pointClass[i] = new PointClass((float)(rand.NextDouble() * count), (float)(rand.NextDouble() * count));
+                pointStructD[i] = new PointStructD(rand.NextDouble() * count, rand.NextDouble() * count);
+                pointStructF[i] = new PointStructF((float)(rand.NextDouble() * count), (float)(rand.NextDouble() * count));
+            }
+        }
+
+        static void BulkGetDistanceOnClass()
+        {
+            for (int i = 0; i < count - 2; i++)
+                _ = pointClass[i].GetDistance(pointClass[i + 1]);
+        }
+
+        static void BulkGetDistanceOnStructF()
+        {
+            for (int i = 0; i < count - 2; i++)
+                _ = pointStructF[i].GetDistance(pointStructF[i + 1]);
+        }
+
+        static void BulkGetDistanceOnStructD()
+        {
+            for (int i = 0; i < count - 2; i++)
+                _ = pointStructD[i].GetDistance(pointStructD[i + 1]);
+        }
+
+        static void BulkGetFastDistanceOnStructF()
+        {
+            for (int i = 0; i < count - 2; i++)
+                _ = pointStructF[i].GetFastDistance(pointStructF[i + 1]);
+        }
+    }
+    #region Point class and structures
+    class PointClass
+    {
+        public float X;
+        public float Y;
+
+        public PointClass(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float GetDistance(PointClass other)
+            => MathF.Sqrt((X - other.X) * (X - other.X) + (Y - other.Y) * (Y - other.Y));
+    }
+
+    struct PointStructD
+    {
+        public double X;
+        public double Y;
+
+        public PointStructD(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double GetDistance(PointStructD other)
+            => Math.Sqrt((X - other.X) * (X - other.X) + (Y - other.Y) * (Y - other.Y));
+    }
+
+    struct PointStructF
+    {
+        public float X;
+        public float Y;
+
+        public PointStructF(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float GetDistance(PointStructF other)
+            => MathF.Sqrt((X - other.X) * (X - other.X) + (Y - other.Y) * (Y - other.Y));
+
+        public float GetFastDistance(PointStructF other)
+            => (X - other.X) * (X - other.X) + (Y - other.Y) * (Y - other.Y);
+    }
+    #endregion
+
+    class MyBenchmarkRunner
+    {
+        public static long Start(Action action)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            action();
+
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+    }
+}
---
+++ AlgoAndDataStructures/Lesson03/temp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Lesson03
+{
+    public class temp
+    {
+        public static void runTemp()
+        {
+            Console.WriteLine(Test(() => SlowSQRT()));
+            Console.WriteLine(Test(() => FastSQRT()));
+            Console.WriteLine("---");
+            Console.WriteLine(Test(() => F()));
+            Console.WriteLine(Test(() => S()));
+        }
+
+        #region MyRegion
+
+        public static long Test(Action func)
+        {
+            const int count = 90_000_000;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < count; i++)
+            {
+                func();
+            }
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+
+        public static double SlowSQRT()
+        {
+            double z = 999;
+            if (z == 0) return 0;
+            return Math.Sqrt(z);
+        }
+
+        public static float FastSQRT()
+        {
+            float z = 999;
+            if (z == 0) return 0;
+            FloatIntUnion u;
+            u.i = 0;
+            u.f = z;
+            u.i -= 1 << 23; /* Subtract 2^m. */
+            u.i >>= 1; /* Divide by 2. */
+            u.i += 1 << 29; /* Add ((b + 1) / 2) * 2^m. */
+            return u.f;
+        }
+
+        public static double F()
+        {
+            int x = 2, y = 3;
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public static double S()
+        {
+            int x = 2, y = 3;
+            return x * x + y * y;
+        }
+
+        [StructLayout(LayoutKind.Explicit, Pack = 1)]
+        public struct FloatIntUnion
+        {
+            [FieldOffset(0)]
+            public int i;
+
+            [FieldOffset(0)]
+            public float f;
+        }
+        #endregion
+    }
+}
