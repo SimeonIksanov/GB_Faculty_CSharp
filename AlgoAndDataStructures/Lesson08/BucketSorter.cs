0c475b1e57313204e4cefd9129bab1509268c4f6
Simeon Iksanov <simeon.iksanov@gmail.com>
2021-07-22 21:30:57 +0500

    Algo and Structures: Lesson 08 Homework

---
+++ AlgoAndDataStructures/Lesson08/BucketSorter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Lesson08
+{
+    public static class BucketSorter
+    {
+
+        public static void BucketSort(this int[] array, int min, int max)
+        {
+            static int FindIndex(int[] array, int bucketCount, int min, int max, int i)
+            { // формулу взял в википедии
+                return (int)Math.Floor((double)(bucketCount * (array[i] - min) / (max - min)));
+            }
+
+            int bucketCount = 10;
+            //int min = 0, max = 100;
+            List<int>[] buckets = new List<int>[bucketCount];
+
+            for (int i = 0; i < bucketCount; i++)
+                buckets[i] = new List<int>();
+
+            for (int i = 0; i < array.Length; i++)
+                buckets[FindIndex(array, bucketCount, min, max, i)].Add(array[i]);
+
+            int index = 0;
+            for (int i = 0; i < bucketCount; i++)
+            {
+                buckets[i].Sort();
+                for (int j = 0; j < buckets[i].Count; j++)
+                    array[index++] = buckets[i][j];
+            }
+
+        }
+    }
+}
---
+++ AlgoAndDataStructures/Lesson08/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = CreateRandomArray(count: 100, maxValue: 100);
+            Console.WriteLine("Unsorted array: ");
+            PrintArray(array);
+
+            array.BucketSort(min: 0, max: 100);
+            Console.WriteLine("Sorted array: ");
+            PrintArray(array);
+        }
+
+        static int[] CreateRandomArray(int count, int maxValue)
+        {
+            Random rnd = new Random();
+            return Enumerable.Range(1, count).Select(x => rnd.Next(maxValue)).ToArray();
+        }
+
+        static void PrintArray(int[] array) =>
+            Console.WriteLine(string.Join(' ', array));
+    }
+}
