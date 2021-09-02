4e7fc148d77ae7849a91648362a64168ee365ed4
Simeon Iksanov <simeon.iksanov@gmail.com>
2021-09-02 17:52:01 +0500

    asp_api_microservices: Homework Lesson01

---
+++ ASP_API_Microservices/Lesson01/Controllers/CrudController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CrudController : ControllerBase
+    {
+        private readonly Dictionary<DateTime, double> _holder;
+
+        public CrudController(Dictionary<DateTime, double> holder)
+        {
+            _holder = holder;
+        }
+
+        [HttpGet]
+        public IActionResult Read([FromQuery] DateTime since, [FromQuery] DateTime until)
+        {
+            if (until == DateTime.MinValue)
+                until = DateTime.MaxValue;
+
+            return Ok(_holder.Where(kvp => since < kvp.Key && kvp.Key < until)
+                   .ToArray());
+        }
+
+        [HttpPut]
+        public IActionResult Add([FromQuery] DateTime dateTime, double temp)
+        {
+            if (_holder.ContainsKey(dateTime))
+                return BadRequest("dateTime already exists");
+
+            _holder.Add(dateTime, temp);
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult Modify([FromQuery] DateTime dateTime, double temp)
+        {
+            if (_holder.ContainsKey(dateTime))
+            {
+                _holder[dateTime] = temp;
+                return Ok();
+            }
+
+            return BadRequest("No datetime found");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromQuery] DateTime dateTime)
+        {
+            if (_holder.ContainsKey(dateTime))
+                _holder.Remove(dateTime);
+            return Ok();
+        }
+    }
+}
