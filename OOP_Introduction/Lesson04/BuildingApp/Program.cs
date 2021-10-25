27836ecf76007d655e66a6fcd013d286f2da52de
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2021-10-25 23:03:41 +0500

    OOP_Introduction: Homework Lesson04

---
+++ OOP_Introduction/Lesson04/BuildingApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BuildingLib;
+
+namespace BuldingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Homework for lesson 05");
+
+            Creator.CreateBuiling();
+            Creator.CreateBuiling(5);
+            Creator.CreateBuiling(5, 3);
+
+            Console.WriteLine($"Построено {Creator.AllBuildings.Count} дома");
+
+            foreach(var b in Creator.AllBuildings.Values)
+            {
+                Console.WriteLine(b);
+            }
+
+            Creator.Remove(2);
+            Console.WriteLine($"Одно здание рухнуло, осталось {Creator.AllBuildings.Count}");
+        }
+    }
+}
---
+++ OOP_Introduction/Lesson04/BuildingLib/Creator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuildingLib
+{
+	public class Creator
+	{
+		public static Dictionary<int, Building> AllBuildings = new Dictionary<int, Building>();
+		private Creator()
+		{
+		}
+
+		public static Building CreateBuiling()
+		{
+			var b = new Building(27, 9, 108, 2);
+			AddToHashtable(b);
+			return b;
+		}
+
+		public static Building CreateBuiling(int numberOfStoreys)
+		{
+			var b = new Building(3 * numberOfStoreys, numberOfStoreys, numberOfStoreys*4, 1);
+			AddToHashtable(b);
+			return b;
+		}
+
+		public static Building CreateBuiling(int numberOfStoreys, int numberOfEntrances)
+		{
+			var b = new Building(
+					height: 3 * numberOfStoreys,
+					numberOfStoreys: numberOfStoreys,
+					numberOfFlats: numberOfStoreys * 4 * numberOfEntrances,
+					numberOfEntrances: numberOfEntrances);
+			AddToHashtable(b);
+			return b;
+		}
+
+		public static void Remove(int id)
+		{
+			if(AllBuildings.ContainsKey(id))
+			{
+				AllBuildings.Remove(id);
+			}
+		}
+		private static void AddToHashtable(Building b)
+		{
+			AllBuildings.Add(b.Id, b);
+		}
+	}
+}
