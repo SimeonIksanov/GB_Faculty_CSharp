3b6245bb747dedccb710b0bda8b03000eecf069d
Simeon Iksanov <simeon.iksanov@gmail.com>
2021-11-21 14:55:29 +0500

    add increment method

--- OOP_Introduction/Lesson04/BuildingLib/Building.cs
+++ OOP_Introduction/Lesson04/BuildingLib/Building.cs
@@ -2,61 +2,66 @@
 
 namespace BuildingLib
 {
-	public class Building
-	{
-		private const int STOREYHEIGHT = 3; //in meters
-		
-		private static int _idCounter = 0;
+    public class Building
+    {
+        private const int STOREYHEIGHT = 3; //in meters
 
-		private readonly int _id;
-		private readonly int _height;
-		private readonly int _numberOfStoreys;
-		private readonly int _numberOfFlats;
-		private readonly int _numberOfEntrances;
+        private static int _idCounter = 0;
 
-		internal Building(int height, int numberOfStoreys, int numberOfFlats, int numberOfEntrances)
-		{
-			_idCounter++;
+        private readonly int _id;
+        private readonly int _height;
+        private readonly int _numberOfStoreys;
+        private readonly int _numberOfFlats;
+        private readonly int _numberOfEntrances;
 
-			_id = _idCounter;
-			_height = height;
-			_numberOfStoreys = numberOfStoreys;
-			_numberOfFlats = numberOfFlats;
-			_numberOfEntrances = numberOfEntrances;
-		}
+        internal Building(int height, int numberOfStoreys, int numberOfFlats, int numberOfEntrances)
+        {
+            IncrementBuildingCounter();
 
-		public int Id { get => _id; }
+            _id = _idCounter;
+            _height = height;
+            _numberOfStoreys = numberOfStoreys;
+            _numberOfFlats = numberOfFlats;
+            _numberOfEntrances = numberOfEntrances;
+        }
 
-		public int Height { get => _height; }
+        private static void IncrementBuildingCounter()
+        {
+            _idCounter++;
+        }
 
-		public int NumberOfStoreys { get => _numberOfStoreys; }
+        public int Id { get => _id; }
 
-		public int NumberOfFlats { get => _numberOfFlats; }
+        public int Height { get => _height; }
 
-		public int NumberOfEntrances { get => _numberOfEntrances; }
+        public int NumberOfStoreys { get => _numberOfStoreys; }
 
-		public int GetHeightOfStorey(int storey)
-		{
-			if (storey <1 || storey > _numberOfStoreys)
-			{
-				throw new ArgumentException("wrong value for storey");
-			}
-			return storey * STOREYHEIGHT;
-		}
+        public int NumberOfFlats { get => _numberOfFlats; }
 
-		public int GetNumberOfFlatsInEntrance()
-		{
-			return _numberOfFlats / _numberOfEntrances;
-		}
+        public int NumberOfEntrances { get => _numberOfEntrances; }
 
-		public int GetNumberOfFlatsInStorey()
-		{
-			return _numberOfFlats / _numberOfStoreys;
-		}
+        public int GetHeightOfStorey(int storey)
+        {
+            if (storey < 1 || storey > _numberOfStoreys)
+            {
+                throw new ArgumentException("wrong value for storey");
+            }
+            return storey * STOREYHEIGHT;
+        }
 
-		public override string ToString()
-		{
-			return $"Building number {Id} is {Height} meters high, has {NumberOfStoreys} storeys, {NumberOfFlats} flats and {NumberOfEntrances} entrances";
-		}
-	}
+        public int GetNumberOfFlatsInEntrance()
+        {
+            return _numberOfFlats / _numberOfEntrances;
+        }
+
+        public int GetNumberOfFlatsInStorey()
+        {
+            return _numberOfFlats / _numberOfStoreys;
+        }
+
+        public override string ToString()
+        {
+            return $"Building number {Id} is {Height} meters high, has {NumberOfStoreys} storeys, {NumberOfFlats} flats and {NumberOfEntrances} entrances";
+        }
+    }
 }
