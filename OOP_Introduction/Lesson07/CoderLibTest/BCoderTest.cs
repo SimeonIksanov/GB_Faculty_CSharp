3ee14dd380078de3b9e23ac5a509dc06f4e4d81c
Simeon Iksanov <simeon.iksanov@gmail.com>
2021-11-26 18:33:03 +0500

    OOP_Introduction: Homework Lesson07 + XUnit tests

---
+++ OOP_Introduction/Lesson07/CoderLibTest/BCoderTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+using CoderLib;
+
+namespace CoderLibTest
+{
+    public class BCoderTest
+    {
+        ICoder _coder;
+
+        public BCoderTest()
+        {
+            _coder = new BCoder();
+        }
+
+        [Fact]
+        void Encode()
+        {
+            Assert.Equal("zyx", _coder.Encode("abc"));
+            Assert.Equal("cba", _coder.Encode("xyz"));
+            Assert.Equal("яа", _coder.Encode("ая"));
+        }
+
+        [Fact]
+        void Decode()
+        {
+            Assert.Equal("zab", _coder.Decode("azy"));
+            Assert.Equal("cba", _coder.Decode("xyz"));
+            Assert.Equal("АЯ", _coder.Decode("ЯА"));
+        }
+    }
+}
--- OOP_Introduction/Lesson07/ConsoleApp/Program.cs
+++ OOP_Introduction/Lesson07/ConsoleApp/Program.cs
@@ -9,31 +9,10 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //Task01();
+            // Task01();
+            // XUnit tests aplied
 
             Task02();
-        }
-
-        static void Task01()
-        {
-            Console.WriteLine("=== ACoder ===");
-            ICoder coder = new ACoder();
-
-            Console.WriteLine("Encode 'amz' -> " + Encrypt(coder, "amz"));
-            Console.WriteLine("Encode 'AMZ' -> " + Encrypt(coder, "AMZ"));
-            Console.WriteLine("Decode 'bna' -> " + Decrypt(coder, "bna"));
-            Console.WriteLine("Decode 'BNA' -> " + Decrypt(coder, "BNA"));
-            Console.WriteLine("Encode 'яма' -> " + Encrypt(coder, "яма"));
-            Console.WriteLine("Decode 'анб' -> " + Decrypt(coder, "анб"));
-
-            Console.WriteLine("\n\n=== BCoder ===");
-            coder = new BCoder();
-            Console.WriteLine("Encode 'amz' -> " + Encrypt(coder, "amz"));
-            Console.WriteLine("Decode 'zna' -> " + Decrypt(coder, "zna"));
-            Console.WriteLine("Encode 'AMZ' -> " + Encrypt(coder, "AMZ"));
-            Console.WriteLine("Decode 'ZNA' -> " + Decrypt(coder, "ZNA"));
-            Console.WriteLine("Encode 'ая' -> " + Encrypt(coder, "ая"));
-            Console.WriteLine("Decode 'АЯ' -> " + Decrypt(coder, "АЯ"));
         }
 
         static void Task02()
