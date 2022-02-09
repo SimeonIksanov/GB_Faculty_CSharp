9c9064f97e3ec1cc2d91d1eac3474de7059e8c0e
Simeon Iksanov <simeon.iksanov@gmail.com>
2022-02-09 23:38:49 +0500

    AspNetCore_WebApp: Homework Lesson03: Add TimeSheetsService + SeriviceTest

---
+++ AspNetCore_WebApp/Timesheets/Service/Repository/IPersonRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Service.Models;
+
+namespace Service.Repository
+{
+    public interface IPersonRepository
+    {
+        Task<Person> AddPersonAsync(Person person, CancellationToken token);
+
+        Task AddPersonRangeAsync(IEnumerable<Person> person, CancellationToken token);
+
+        Task<Person> GetPersonById(int id, CancellationToken token);
+
+        Task<IEnumerable<Person>> GetPersonByNameAsync(string name, PersonParameters personParameters, CancellationToken token);
+
+        Task<IEnumerable<Person>> GetPersonsAsync(PersonParameters personParameters, CancellationToken token);
+
+        Task UpdatePerson(Person person, CancellationToken token);
+
+        Task DeletePerson(int id, CancellationToken token);
+    }
+}
---
+++ AspNetCore_WebApp/Timesheets/Service/Repository/PersonRepositoryInMemory.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Service.Models;
+
+namespace Service.Repository
+{
+    public class PersonRepositoryInMemory : IPersonRepository
+    {
+        private List<Person> _storage;
+        private int _lastId = 1;
+        private Object lockObject = new object();
+
+        public PersonRepositoryInMemory()
+        {
+            _storage = new List<Person>(50);
+        }
+
+        public async Task<Person> AddPersonAsync(Person person, CancellationToken token)
+        {
+            person.Id = _lastId++;
+            await Task.Run(() => _storage.Add(person));
+            return person;
+        }
+
+        public async Task AddPersonRangeAsync(IEnumerable<Person> persons, CancellationToken token)
+        {
+            foreach (var person in persons)
+            {
+                person.Id = _lastId++;
+                await Task.Run(() => _storage.Add(person));
+            }
+        }
+
+
+        public async Task<Person> GetPersonById(int id, CancellationToken token)
+        {
+            return await Task.Run(() => _storage.Where(p => p.Id == id).DefaultIfEmpty(new Person()).First(), token);
+        }
+
+        public async Task<IEnumerable<Person>> GetPersonByNameAsync(string name, PersonParameters personParameters, CancellationToken token)
+        {
+            return await Task.Run(() => _storage.Where(p => p.FirstName == name)
+                                                .Skip((personParameters.PageNumber - 1) * personParameters.PageSize)
+                                                .Take(personParameters.PageSize)
+                                                .ToList(),
+                                  token);
+        }
+
+        public async Task<IEnumerable<Person>> GetPersonsAsync(PersonParameters personParameters, CancellationToken token)
+        {
+            return await Task.Run(() => _storage.Skip((personParameters.PageNumber - 1) * personParameters.PageSize)
+                                                .Take(personParameters.PageSize)
+                                                .ToList(),
+                                  token);
+        }
+
+
+        public async Task UpdatePerson(Person person, CancellationToken token)
+        {
+            await Task.Run(() =>
+            {
+                var personInRepo = _storage.First(p => p.Id == person.Id);
+
+                // специально не использую Automapper
+                personInRepo.FirstName = person.FirstName;
+                personInRepo.LastName = person.LastName;
+                personInRepo.Company = person.Company;
+                personInRepo.Age = person.Age;
+                personInRepo.Email = person.Email;
+            }, token);
+        }
+
+        public async Task DeletePerson(int id, CancellationToken token)
+        {
+            await Task.Run(() => _storage.Remove(_storage.First(p => p.Id == id)), token);
+        }
+    }
+}
