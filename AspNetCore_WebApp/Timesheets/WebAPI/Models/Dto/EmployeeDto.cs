0ae14e3c0dcebc3ef5cfa5957915898e297c809c
Simeon Iksanov <simeon.iksanov@gmail.com>
2022-02-25 20:09:00 +0500

    AspNetCore_WebApp: Homework Lesson06

---
+++ AspNetCore_WebApp/Timesheets/WebAPI/Models/Dto/EmployeeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WebAPI.ValidationAttributes;
+
+namespace Models.Entities
+{
+    public class EmployeeDto
+    {
+        [Required, NotEmpty()]
+        public Guid Id { get; set; }
+
+        [Required, NotEmpty()]
+        public Guid UserId { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public ICollection<Sheet> Sheets { get; set; }
+
+    }
+}
