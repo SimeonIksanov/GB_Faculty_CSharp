0c1893965edc4af050c0b5383776d5899e824d86
Simeon Iksanov <simeon.iksanov@gmail.com>
2022-02-25 20:38:10 +0500

    AspNetCore_WebApp: Homework Lesson06+Summary on interfaces

--- AspNetCore_WebApp/Timesheets/Domain/Managers/Interfaces/IUserManager.cs
+++ AspNetCore_WebApp/Timesheets/Domain/Managers/Interfaces/IUserManager.cs
@@ -10,8 +10,21 @@ namespace Domain.Managers.Interfaces
 {
     public interface IUserManager
     {
+        /// <summary>
+        /// Create a new user, hash its password and save to DB
+        /// </summary>
         Task CreateUser(CreateUserRequest createUserRequest, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Searches a user in db
+        /// </summary>
+        /// <returns>User instance</returns>
         Task<User> GetUser(LoginRequest request, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Searches a user by refresh token
+        /// </summary>
+        /// <returns>User instance</returns>
         Task<User> FindUserByToken(string token, CancellationToken cancellationToken);
     }
 }
