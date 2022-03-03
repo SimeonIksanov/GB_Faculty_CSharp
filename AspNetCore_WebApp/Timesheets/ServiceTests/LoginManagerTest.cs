54567ecc133fc03bbccfe96c78dbe14fe64bcce9
Simeon Iksanov <simeon.iksanov@gmail.com>
2022-03-04 01:05:34 +0500

    AspNetCore_WebApp: Homework Lesson08

---
+++ AspNetCore_WebApp/Timesheets/ServiceTests/LoginManagerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Data.Interfaces;
+using Domain.Managers.Implementation;
+using Domain.Managers.Interfaces;
+using Models.Auth;
+using Models.Entities;
+using Moq;
+using Xunit;
+
+namespace ServiceTests
+{
+    public class LoginManagerTest
+    {
+        private ILoginManager _loginManager;
+
+        public LoginManagerTest()
+        {
+            var tokenServiceMock = new Mock<ITokenService>();
+            tokenServiceMock
+                .Setup<string>(ts => ts.GenerateAccessToken(new List<Claim>()))
+                .Returns("accessToken");
+            tokenServiceMock
+                .Setup<string>(ts => ts.GenerateRefreshToken(new List<Claim>()))
+                .Returns("RefreshToken");
+
+            _loginManager = new LoginManager(tokenServiceMock.Object);
+        }
+
+        [Fact]
+        public void AuthTest()
+        {
+            User tmpUser = new User
+            {
+                Id = Guid.NewGuid(),
+                Username = "aaaa"
+            };
+            var userRepoMock = new Mock<IUserRepository>();
+            userRepoMock
+                .Setup(ur => ur.Update(tmpUser));
+
+            _loginManager.Authenticate(userRepoMock.Object, tmpUser);
+
+            userRepoMock.Verify(mock => mock.Update(tmpUser), Times.AtLeastOnce);
+        }
+    }
+}
