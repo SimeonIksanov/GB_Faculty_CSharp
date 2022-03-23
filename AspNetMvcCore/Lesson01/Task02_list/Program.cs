71f9f0e911e925fb528802e9ec40698a98bb9b57
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2022-03-24 00:27:22 +0500

    AspNetMvcCore: hw01

---
+++ AspNetMvcCore/Lesson01/Task02_list/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Task02_list;
+
+namespace Task02_List
+{
+    class Program
+    {
+        public static void Main()
+        {
+            MyList<int> testMyList = new MyList<int>();
+            List<int> testList = new List<int>();
+
+            StartParallel(() => AddMethod(testMyList));
+            Wait();
+            Console.WriteLine("my list total count: {0}",testMyList.Count);
+
+
+            StartParallel(() => AddMethod(testList));
+            Wait();
+            Console.WriteLine("original list total count: {0}", testList.Count);
+        }
+
+        static void StartParallel(Action action)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(_ => action());
+            }
+        }
+
+        static void AddMethod(IList<int> list)
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                list.Add(i);
+            }
+        }
+
+        static void Wait()
+        {
+            if (ThreadPool.PendingWorkItemCount != 0)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
