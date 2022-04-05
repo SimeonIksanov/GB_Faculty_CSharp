875151a70de6096154b5d336ab015f25e0c2fb24
Simeon Iksanov <simeon.iksanov@gmail.com>
2022-04-05 20:46:53 +0500

    AspNetMvcCore: hw04

---
+++ AspNetMvcCore/Lesson04/Task01/BathSection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task01
+{
+    public class BathSection
+    {
+        public List<BathProduct> Products { get; set; }
+    }
+
+    public class BathProduct
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Color { get; set; }
+    }
+}
