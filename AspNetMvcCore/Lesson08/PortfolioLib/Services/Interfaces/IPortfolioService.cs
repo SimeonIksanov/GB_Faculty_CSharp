38e2da3771ee4df66a4f5e04ef0df65039043e5a
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2022-04-21 00:44:42 +0500

    AspNetMvcCore: homework 08

---
+++ AspNetMvcCore/Lesson08/PortfolioLib/Services/Interfaces/IPortfolioService.cs
@@ -0,0 +1,21 @@
+﻿using PortfolioLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioLib.Services.Interfaces
+{
+    public interface IPortfolioService
+    {
+        Portfolio Create(Portfolio item);
+
+        IEnumerable<Portfolio> GetAll();
+        Portfolio GetById (int id);
+
+        Portfolio Update(int id, Portfolio item);
+
+        void Delete(int id);
+    }
+}
---
+++ AspNetMvcCore/Lesson08/WebApp/ViewModels/PortfolioViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WebApp.Controllers;
+
+namespace WebApp.ViewModels
+{
+    public class PortfolioViewModel
+    {
+        public int PortfolioId { get; set; }
+
+        [Required]
+        [StringLength(6,ErrorMessage = "Length not more 6 ch")]
+        [Display(Name ="Account name")]
+        public string Account { get; set; }
+
+        public virtual ICollection<StockViewModel> Stocks { get; set; }
+    }
+}
